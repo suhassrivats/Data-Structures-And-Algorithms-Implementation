2120c7a54299b929e4a6e0aa8e89529b73565a18
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-06-09 22:46:57 -0700

    correct circular_queue_using_arrays implementation

--- DataStructures/Queues/Implementation/circular_queue_using_arrays.py
+++ DataStructures/Queues/Implementation/circular_queue_using_arrays.py
@@ -1,70 +1,84 @@
-# Implement Circular Queue using Arrays
+class MyCircularQueue:
 
-
-class CircularQueue:
-
-    def __init__(self, c):
-        self.capacity = c
-        self.queue = []
+    def __init__(self, k: int):
+        """
+        Initialize your data structure here. Set the size of the queue to be k.
+        """
+        self.capacity = k
+        self.queue = [None] * k
         self.front = -1
         self.rear = -1
 
-    def is_empty(self):
+    def enQueue(self, value: int) -> bool:
+        """
+        Insert an element into the circular queue. Return true if the operation
+        is successful.
+        """
+        if self.isEmpty():
+            self.front = self.rear = 0
+        elif self.isFull():
+            return False
+        else:
+            # next position of rear
+            self.rear = (self.rear+1) % self.capacity
+        # append from rear end
+        self.queue[self.rear] = value
+        return True
+
+    def deQueue(self) -> bool:
+        """
+        Delete an element from the circular queue. Return true if the operation
+        is successful.
+        """
+        if self.isEmpty():
+            return False
+        # When there is only one element
+        elif self.rear == self.front:
+            self.rear = self.front = -1
+        else:
+            # remove from front end
+            self.front = (self.front+1) % self.capacity
+        return True
+
+    def Front(self) -> int:
+        """
+        Get the front item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.front]
+
+    def Rear(self) -> int:
+        """
+        Get the last item from the queue.
+        """
+        if self.isEmpty():
+            return -1
+        return self.queue[self.rear]
+
+    def isEmpty(self) -> bool:
+        """
+        Checks whether the circular queue is empty or not.
+        """
         if self.rear == self.front == -1:
             return True
         else:
             return False
 
-    def is_full(self):
+    def isFull(self) -> bool:
+        """
+        Checks whether the circular queue is full or not.
+        """
         if ((self.rear+1) % self.capacity) == self.front:
             return True
         else:
             return False
 
-    def enqueue(self, item):
-        if self.is_full():
-            print('Queue is full')
-            return
-        elif self.is_empty():
-            print('Queue is empty')
-            self.front = self.rear = 0
-        else:
-            self.rear = ((self.rear+1) % self.capacity)
-        self.queue.append(item)
-
-    def dequeue(self):
-        # Condition for an empty array
-        if self.is_empty():
-            return
-        # Condition for only one element
-        elif self.rear == self.front:
-            self.rear = self.front = -1
-        else:
-            self.front = (self.front + 1) % self.capacity
-
-    def display(self):
-
-        i = self.front
-        if self.is_empty():
-            print('Queue is empty')
-        else:
-            print('Queue is:')
-            while(i is not self.rear):
-                print(self.queue[i])
-                i = (i+1) % self.capacity
-            print(self.queue[i])
-
-
-## Driver function ##
-q = CircularQueue(4)
-q.enqueue(20)
-q.enqueue(30)
-q.enqueue(40)
-q.enqueue(50)
-q.enqueue(90)
-q.display()
-q.dequeue()
-q.dequeue()
-q.dequeue()
-q.dequeue()
-q.display()
+# Your MyCircularQueue object will be instantiated and called as such:
+# obj = MyCircularQueue(k)
+# param_1 = obj.enQueue(value)
+# param_2 = obj.deQueue()
+# param_3 = obj.Front()
+# param_4 = obj.Rear()
+# param_5 = obj.isEmpty()
+# param_6 = obj.isFull()
