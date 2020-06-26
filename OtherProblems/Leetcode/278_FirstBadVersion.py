dbdffdde6bcafbb5d972f47f7d85e29c1c009675
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-06-26 13:46:51 -0700

    feat: LC278 FirstBadVersion algorithm

---
+++ OtherProblems/Leetcode/278_FirstBadVersion.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        left = 1
+        right = n
+
+        while left < right:
+            mid = (left + right) // 2
+            res = isBadVersion(mid)
+
+            if res:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
