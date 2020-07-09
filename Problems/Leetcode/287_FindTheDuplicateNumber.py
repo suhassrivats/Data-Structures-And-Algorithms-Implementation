30779b10c4a655da9021949fc8cfebc8765090c7
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-07-08 22:11:35 -0700

    LC287 implement alternate algorithm

--- Problems/Leetcode/287_FindTheDuplicateNumber.py
+++ Problems/Leetcode/287_FindTheDuplicateNumber.py
@@ -5,3 +5,11 @@ class Solution:
             if num in seen:
                 return num
             seen.add(num)
+
+
+# class Solution:
+#     def findDuplicate(self, nums):
+#         nums.sort()
+#         for i in range(1, len(nums)):
+#             if nums[i] == nums[i-1]:
+#                 return nums[i]
