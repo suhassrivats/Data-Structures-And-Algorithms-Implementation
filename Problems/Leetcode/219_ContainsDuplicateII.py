a1a64e0ae39cd9273ead26bb8fb6c1103dc5359e
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-07-11 21:40:21 -0700

    feat: LC219_ContainsDuplicateII algorithm

---
+++ Problems/Leetcode/219_ContainsDuplicateII.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dict = {}
+        for i, v in enumerate(nums):
+            if (v in dict) and (i - dict[v] <= k):
+                return True
+            dict[v] = i
+        return False
