91549bf396370968ba103fbcb7a33d162d462fe5
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-07-24 22:42:16 -0700

    feat: LC106 Construct Binary Tree from Inorder and Postorder Traversal

---
+++ Problems/Leetcode/106_ConstructBinaryTreeFromInorderAndPostorderTraversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+        if inorder:
+            ind = inorder.index(postorder.pop())
+            root = TreeNode(inorder[ind])
+            root.right = self.buildTree(inorder[ind+1:], postorder)
+            root.left = self.buildTree(inorder[:ind], postorder)
+            return root
