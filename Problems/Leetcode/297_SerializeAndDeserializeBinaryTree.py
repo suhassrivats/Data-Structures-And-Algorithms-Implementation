cea5905f6820f791a717d4623c5aeaf84533a31f
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-07-27 23:43:08 -0700

    feat: LC297 Serialize and Deserialize Binary Tree algorithm

---
+++ Problems/Leetcode/297_SerializeAndDeserializeBinaryTree.py
@@ -0,0 +1,63 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import deque
+
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        q = deque([root])
+        res = []
+
+        while q:
+            node = q.popleft()
+            if node:
+                q.append(node.left)
+                q.append(node.right)
+            res.append(str(node.val) if node else '#')
+
+        return ','.join(res)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+
+        nodes = data.split(',')
+        root = TreeNode(int(nodes[0]))
+        q = deque([root])
+        index = 1
+
+        while q:
+            node = q.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                q.append(node.left)
+            index += 1
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                q.append(node.right)
+            index += 1
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
