281a639064efc81f82b414d4fb1e39221f7a01eb
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-07-28 20:50:06 -0700

    LC344_ReverseString using recursion

--- Problems/Leetcode/344_ReverseString.py
+++ Problems/Leetcode/344_ReverseString.py
@@ -7,3 +7,13 @@ class Solution:
         for i in range(l//2):
             s[i], s[l-i-1] = s[l-i-1], s[i]
         return s
+
+
+# Using Recursion
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left+1, right-1)
+        helper(0, len(s)-1)
