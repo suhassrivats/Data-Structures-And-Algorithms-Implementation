2c30a04b5977b846b1d6732340acf01a4a550dd7
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-08-03 22:41:16 -0700

    feat: implement LC100_SameTree algorithm

---
+++ Problems/Leetcode/100_SameTree.py
@@ -0,0 +1,45 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+# Recursion
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and \
+            self.isSameTree(p.left, q.left)
+
+
+# Iterative
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+
+        deq = deque([(p, q), ])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+
+        return True
