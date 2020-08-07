2123781f6373413d13250edfc0e2788b818e57f1
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-08-07 00:53:27 -0700

    feat: LC235_LowestCommonAncestorOfABinarySearchTree algorithm

---
+++ Problems/Leetcode/235_LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,71 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# Iterative
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        Complexity Analysis:
+        Time Complexity: O(N), where N is the number of nodes in the BST.
+        In the worst case we might be visiting all the nodes of the BST.
+
+        Space Complexity: O(N). This is because the maximum amount of space
+        utilized by the recursion stack would be N since the height of a
+        skewed BST could be N.
+        """
+
+        parent_val = root.val
+        p_val = p.val
+        q_val = q.val
+
+        # If both p and q are greater than parent
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        # If both p and q are lesser than parent
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+
+        else:
+            return root
+
+
+# Recursive
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+
+        # Value of p
+        p_val = p.val
+
+        # Value of q
+        q_val = q.val
+
+        # Start from the root node of the tree
+        node = root
+
+        # Traverse the tree
+        while node:
+
+            # Value of current node or parent node.
+            parent_val = node.val
+
+            if p_val > parent_val and q_val > parent_val:
+                # If both p and q are greater than parent
+                node = node.right
+            elif p_val < parent_val and q_val < parent_val:
+                # If both p and q are lesser than parent
+                node = node.left
+            else:
+                # We have found the split point, i.e. the LCA node.
+                return node
