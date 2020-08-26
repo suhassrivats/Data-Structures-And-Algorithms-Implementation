f778fb04af1af18afc3ff46f27501930ff138c40
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-08-26 00:33:27 -0700

    LC543_DiameterOfBinaryTree algorithm

---
+++ Problems/Leetcode/543_DiameterOfBinaryTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 0
+
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.ans = max(self.ans, left+right)
+            return 1 + max(left, right)
+
+        dfs(root)
+        return self.ans
