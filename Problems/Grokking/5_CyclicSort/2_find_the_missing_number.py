3323992fbdb6808c46ef14d4195df638387c33b4
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-03 20:24:08 -0700

    optimize LC_268_MissingNumber algorithm

--- Problems/Grokking/5_CyclicSort/2_find_the_missing_number.py
+++ Problems/Grokking/5_CyclicSort/2_find_the_missing_number.py
@@ -1,26 +1,4 @@
 class Solution:
-    def missingNumber(self, nums):
-        """
-        Time Complexity: O(n*logn)
-        Space Complexity: O(n) for sorting
-        """
-        nums.sort()
-
-        # Ensure that n is at the last index
-        if nums[-1] != len(nums):
-            return len(nums)
-        # Ensure that 0 is at the first index
-        elif nums[0] != 0:
-            return 0
-
-        # If we get here, then the missing number is on the range (0, n)
-        for i in range(1, len(nums)):
-            expected_num = nums[i-1] + 1
-            if nums[i] != expected_num:
-                return expected_num
-
-
-class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         """
         Time complexity:
