8920fb8e405f4864fca96fae7eb0076e53ba12e8
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-04 00:35:47 -0700

    optimize LC_287_FindTheDuplicateNumber algorithm

--- Problems/Leetcode/287_FindTheDuplicateNumber.py
+++ Problems/Leetcode/287_FindTheDuplicateNumber.py
@@ -1,10 +1,30 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return num
-            seen.add(num)
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+        i = 0
+        while i < len(nums):
+            if nums[i] != i+1:
+                j = nums[i]-1
+                # index and value should be equal. Swap otherwise
+                if nums[i] != nums[j]:
+                    nums[i], nums[j] = nums[j], nums[i]
+                else:
+                    return nums[i]
+            else:
+                i += 1
+        return -1
+
+
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         seen = set()
+#         for num in nums:
+#             if num in seen:
+#                 return num
+#             seen.add(num)
 
 
 # class Solution:
