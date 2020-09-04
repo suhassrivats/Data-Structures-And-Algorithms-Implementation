1d0d6e99758250bb48b3ee7810d6b2a8bdbca0dc
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-03 23:33:47 -0700

    grokking: find all missing numbers algorithm

---
+++ Problems/Grokking/5_CyclicSort/3_find_all_missing_numbers.py
@@ -0,0 +1,25 @@
+def find_missing_numbers(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    missingNumbers = []
+
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            missingNumbers.append(i + 1)
+
+    return missingNumbers
+
+
+def main():
+    print(find_missing_numbers([2, 3, 1, 8, 2, 3, 5, 1]))
+    print(find_missing_numbers([2, 4, 1, 2]))
+    print(find_missing_numbers([2, 3, 2, 1]))
+
+
+main()
