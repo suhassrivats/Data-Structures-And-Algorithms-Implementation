c5868b64918c8d10351309d60973dd4f5775bfa0
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-07 16:50:56 -0700

    LC102: add time and space complexity analysis

--- Problems/Leetcode/102_BinaryTreeLevelOrderTraversal.py
+++ Problems/Leetcode/102_BinaryTreeLevelOrderTraversal.py
@@ -1,12 +1,28 @@
 # Level order is Breadth First traversal
 from collections import deque
 
+
+"""
+Time complexity:
+    The time complexity of the this algorithm is O(N), where ‘N’ is the total
+number of nodes in the tree. This is due to the fact that we traverse each node
+once.
+
+Space complexity:
+    The space complexity of this algorithm will be O(N) as we need to
+return a list containing the level order traversal. We will also need O(N)
+space for the queue. Since we can have a maximum of N/2 nodes at any level
+(this could happen only at the lowest level), therefore we will need O(N)
+space to store them in the queue.
+"""
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 
 # Iterative
