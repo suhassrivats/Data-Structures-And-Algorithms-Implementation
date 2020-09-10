160142f6afeb9c6f3398685afc9bf16007952f0a
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-09 23:49:02 -0700

    DFS introduction and LC112 algorithm

--- Problems/Leetcode/112_PathSum.py
+++ Problems/Leetcode/112_PathSum.py
@@ -5,6 +5,8 @@
 #         self.left = left
 #         self.right = right
 
+
+# Recursive
 class Solution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         if not root:
@@ -15,3 +17,21 @@ class Solution:
 
         return self.hasPathSum(root.left, sum-root.val) or \
             self.hasPathSum(root.right, sum-root.val)
+
+
+# Iterative: DFS + Stack
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        stack = [(root, root.val)]
+        while stack:
+            node, value = stack.pop()
+            if (value == sum) and (not node.left and not node.right):
+                return True
+            if node.right:
+                stack.append((node.right, value+node.right.val))
+            if node.left:
+                stack.append((node.left, value+node.left.val))
+
+        return False
