e3eccacfc6b23e013c916beb2d829d7a4efbea3a
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-09-12 12:40:34 -0700

    rewrite LC543_DiameterOfBinaryTree algorithm

--- Problems/Leetcode/543_DiameterOfBinaryTree.py
+++ Problems/Leetcode/543_DiameterOfBinaryTree.py
@@ -7,16 +7,36 @@
 
 
 class Solution:
+    def __init__(self):
+        self.tree_diameter = 0
+
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        self.ans = 0
+        """
+        Time complexity #
+            The time complexity of the above algorithm is O(N), where ‘N’ is
+        the total number of nodes in the tree. This is due to the fact that we
+        traverse each node once.
 
-        def dfs(node):
-            if not node:
-                return 0
-            left = dfs(node.left)
-            right = dfs(node.right)
-            self.ans = max(self.ans, left+right)
-            return 1 + max(left, right)
+        Space complexity:
+            The space complexity of the above algorithm will be O(N) in the
+        worst case. This space will be used to store the recursion stack. The
+        worst case will happen when the given tree is a linked list (i.e.,
+        every node has only one child).
+        """
 
-        dfs(root)
-        return self.ans
+        self.calculate_height(root)
+        return self.tree_diameter
+
+    def calculate_height(self, current_node):
+        if not current_node:
+            return 0
+
+        left_tree_height = self.calculate_height(current_node.left)
+        right_tree_height = self.calculate_height(current_node.right)
+
+        # update global tree_diameter
+        self.tree_diameter = max(self.tree_diameter, left_tree_height+right_tree_height)
+
+        # height of the current node will be equal to the maximum of the heights
+        # of left or right subtrees plus '1' for the current node
+        return max(left_tree_height + right_tree_height) + 1
