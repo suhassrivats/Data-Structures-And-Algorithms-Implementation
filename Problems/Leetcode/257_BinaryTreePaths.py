ecbb2845134c02be10a9516606400959a12667b6
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-10-03 23:52:05 -0700

    LC257 add recursive algorithm

--- Problems/Leetcode/257_BinaryTreePaths.py
+++ Problems/Leetcode/257_BinaryTreePaths.py
@@ -24,3 +24,21 @@ class Solution:
             if node.left:
                 stack.append((node.left, ls+str(node.val)+'->'))
         return results
+
+
+# dfs recursively
+class Solution:
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        if not root:
+            return []
+        res = []
+        self.dfs(root, "", res)
+        return res
+
+    def dfs(self, root, ls, res):
+        if not root.left and not root.right:
+            res.append(ls+str(root.val))
+        if root.left:
+            self.dfs(root.left, ls+str(root.val)+"->", res)
+        if root.right:
+            self.dfs(root.right, ls+str(root.val)+"->", res)
