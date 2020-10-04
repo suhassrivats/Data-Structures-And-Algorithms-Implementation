7b109130628b0c7fbc49b398dc63dded5b845e30
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-10-03 18:41:08 -0700

    LC1022 alternate solutions

--- Problems/Leetcode/1022_SumOfRootToLeafBinaryNumbers.py
+++ Problems/Leetcode/1022_SumOfRootToLeafBinaryNumbers.py
@@ -1,3 +1,6 @@
+import collections
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,7 +9,7 @@
 #         self.right = right
 
 
-# Recursive
+# Recursive - using BitWise trick
 class Solution:
     """
     Time complexity: O(N), where N is a number of nodes, since one has to visit
@@ -25,3 +28,34 @@ class Solution:
         if not root.left and not root.right:
             return total
         return self.helper(root.left, total) + self.helper(root.right, total)
+
+
+# Recursive - DFS approach
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        return self.dfs(root, 0)
+
+    def dfs(self, root, pre):
+        if not root:
+            return 0
+        pre = pre*2 + root.val
+        if not root.left and not root.right:
+            return pre
+        return self.dfs(root.left, pre) + self.dfs(root.right, pre)
+
+
+# Iterative
+class Solution:
+    def sumRootToLeaf(self, root: TreeNode) -> int:
+        deque = collections.deque()
+        results = 0
+        deque.append((root, 0))
+        while deque:
+            node, path = deque.popleft()
+            if node:
+                if not node.left and not node.right:
+                    results += path*2 + node.val
+                path = path*2 + node.val
+                deque.append((node.left, path))
+                deque.append((node.right, path))
+        return results
