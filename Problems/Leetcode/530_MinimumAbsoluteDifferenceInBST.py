9e108f6677bb7465e64a8bdfb167a2c1f12715dd
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-10-05 00:27:13 -0700

    LC530_MinimumAbsoluteDifferenceInBST algorithm using preorder traversal

---
+++ Problems/Leetcode/530_MinimumAbsoluteDifferenceInBST.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        stack = []
+        previous, min_diff = None, float('inf')
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return min_diff
+            node = stack.pop()
+            if previous:
+                min_diff = min(min_diff, node.val-previous.val)
+            previous = node
+            root = node.right
