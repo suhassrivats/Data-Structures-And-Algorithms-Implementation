b8f48313154f008929b894b42d88250c311c3bcf
Suhas Srivats Subburathinam <subburat@synopsys.com>
2020-10-15 11:31:21 -0700

    LC814_BinaryTreePruning algorithm

---
+++ Problems/Leetcode/814_BinaryTreePruning.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    """
+    Complexity Analysis:
+        Time Complexity: O(N), where NN is the number of nodes in the tree. We
+            process each node once.
+        Space Complexity: O(H), where HH is the height of the tree. This
+            represents the size of the implicit call stack in our recursion.
+    """
+
+    def pruneTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+
+        # If it is a leaf node and its value is 0, then set it as None
+        if not root.left and not root.right and root.val == 0:
+            return None
+
+        return root
