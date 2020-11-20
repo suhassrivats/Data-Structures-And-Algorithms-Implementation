4de8eed2f25faeac79fd44b6f55f508ee244a3d5
suhassrivats <suhassrivats@gmail.com>
2020-11-19 22:15:02 -0800

    LC643: add bruteforce solution

--- Problems/Leetcode/643_MaximumAverageSubarrayI.py
+++ Problems/Leetcode/643_MaximumAverageSubarrayI.py
@@ -1,4 +1,27 @@
+# Brute Force
 class Solution:
+    """
+    Time complexity: O(n * k)
+    Space complexity (auxiliary): O(1)
+    """
+
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        result = []
+        for i in range(len(nums)-k+1):
+            total = 0
+            for j in range(i, i+k):
+                total += nums[j]
+            result.append(total/k)
+        return max(result)
+
+
+# Optimized - Sliding window
+class Solution:
+    """
+    Time complexity: O(n)
+    Space complexity (auxiliary): O(1)
+    """
+
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         result = float('-inf')
         window_start, window_sum = 0, 0.0
