97fbfe883ef7bd79bbd81e4fd84cd60db747afbb
suhassrivats <suhassrivats@gmail.com>
2020-11-19 23:51:04 -0800

    LC904 add intuition and complexity analysis

--- Problems/Leetcode/904_FruitIntoBaskets.py
+++ Problems/Leetcode/904_FruitIntoBaskets.py
@@ -1,4 +1,24 @@
 class Solution:
+    """
+    Intuition:
+        This problem follows the Sliding Window pattern and is quite similar to
+    Longest Substring with K Distinct Characters. In this problem, we need to
+    find the length of the longest subarray with no more than two distinct
+    characters (or fruit types!). This transforms the current problem into
+    Longest Substring with K Distinct Characters where K=2.
+
+    Time Complexity:
+        The above algorithm’s time complexity will be O(N), where ‘N’ is the
+    number of characters in the input array. The outer for loop runs for all
+    characters, and the inner while loop processes each character only once;
+    therefore, the time complexity of the algorithm will be O(N+N), which is
+    asymptotically equivalent to O(N).
+
+    Space Complexity:
+        The algorithm runs in constant space O(1) as there can be a maximum of
+    three types of fruits stored in the frequency map.
+    """
+
     def totalFruit(self, tree):
         window_start = 0
         max_length = 0
@@ -17,4 +37,5 @@ class Solution:
                     del fruit_frequency[left_fruit]
                 window_start += 1  # shrink the window
             max_length = max(max_length, window_end-window_start + 1)
+
         return max_length
