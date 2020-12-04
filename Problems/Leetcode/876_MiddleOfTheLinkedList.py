d2cbbdab0cec0d42d4b36a1c23628c0358a7546a
suhassrivats <suhassrivats@gmail.com>
2020-12-03 22:41:43 -0800

    LC876 add alternate solution

--- Problems/Leetcode/876_MiddleOfTheLinkedList.py
+++ Problems/Leetcode/876_MiddleOfTheLinkedList.py
@@ -4,6 +4,17 @@
 #         self.val = val
 #         self.next = next
 
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        """
+        Time Complexituy: O(N) where N is the number of nodes in a given list
+        Space Complexity: O(N), space used by A. We store all given nodes to A
+        """
+        A = [head]
+        while A[-1].next:
+            A.append(A[-1].next)
+        return A[len(A)//2]
+
 
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
