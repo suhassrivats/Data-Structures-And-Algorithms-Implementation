d1e0396aace58bb6be87bc6299a0f1b5a31024a2
suhassrivats <suhassrivats@gmail.com>
2020-12-05 15:56:47 -0800

    LC977 correct code

--- Problems/Leetcode/977_SquaresOfASortedArray.py
+++ Problems/Leetcode/977_SquaresOfASortedArray.py
@@ -1,12 +1,14 @@
 class Solution:
-    def findNumbers(self, nums: List[int]) -> int:
-        """
-        Time Complexity: O(n)
-        Space Complexity (auxiliary): O(1)
-        """
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        results = [None] * len(A)
+        left, right = 0, len(A)-1
 
-        even_dig_counter = 0
-        for num in nums:
-            if len(str(num)) % 2 == 0:
-                even_dig_counter += 1
-        return even_dig_counter
+        for i in range(len(A)-1, -1, -1):  # Iterate from the end
+            if abs(A[left]) < abs(A[right]):
+                results[i] = A[right] ** 2
+                right -= 1
+            else:
+                results[i] = A[left] ** 2
+                left += 1
+
+        return results
