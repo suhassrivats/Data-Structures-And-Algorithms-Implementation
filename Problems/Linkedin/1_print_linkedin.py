6f112b0c937f4610c07cac4057c1e18b7e0d575f
suhassrivats <suhassrivats@gmail.com>
2020-12-10 23:37:19 -0800

    linkedin SRE prep

--- Problems/Linkedin/1_print_linkedin.py
+++ Problems/Linkedin/1_print_linkedin.py
@@ -7,12 +7,20 @@ divisible by 4, print "Linked" instead. When it would print
  4 and 6, print "LinkedIn."
 '''
 
+#!/usr/bin/python
+
 
 def print_linkedin():
+    """
+    Time Complexity: O(n) where n is the range. Since n=100 (constant) in this
+        case, we can argue that complexity is O(1)
+    Space Complexity: O(1)
+    """
 
     for i in range(1, 101):
         # LCM of 4,6 is 12
-        if i % 12 == 0:
+        # if i % 12 == 0:
+        if i % 4 == 0 and i % 6 == 0:
             print('LinkedIn')
         elif i % 4 == 0:
             print('Linked')
