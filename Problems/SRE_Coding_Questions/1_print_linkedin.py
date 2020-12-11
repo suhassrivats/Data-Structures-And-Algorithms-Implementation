1ebaa2500f3e2923cfb457f34630ae7298fddbbb
suhassrivats <suhassrivats@gmail.com>
2020-12-10 23:55:41 -0800

    style: multi-line comments

--- Problems/SRE_Coding_Questions/1_print_linkedin.py
+++ Problems/SRE_Coding_Questions/1_print_linkedin.py
@@ -1,9 +1,9 @@
-'''
+"""
 Write a program which prints out all numbers between 1 and 100. When the program
 would print out a number exactly divisible by 4, print "Linked" instead. When
 it would print out a number exactly divisible by 6, print "In" instead. When it
 would print out a number exactly divisible by both 4 and 6, print "LinkedIn."
-'''
+"""
 
 #!/usr/bin/python
 
