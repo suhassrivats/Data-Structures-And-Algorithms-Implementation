bec2f49f33b94b37bce24a3cd349429975056821
suhassrivats <suhassrivats@gmail.com>
2021-05-17 16:38:24 -0700

    TikTok: 24_GoodBad.py improve formatting

--- Problems/SRE_Coding_Questions/24_GoodBad.py
+++ Problems/SRE_Coding_Questions/24_GoodBad.py
@@ -1,50 +1,47 @@
-
 '''
-Giving an array with only "g" (means good) and "b" (means bad), "g"s are always before "b"s, and will always have both "g" and "b". Find the index of first "b"
-Or explain it another way: you are giving a revision history, find the first bad revision. "g" stands for good revision and "b" stands for bad revision.
-Example:
+Giving an array with only "g" (means good) and "b" (means bad), "g"s are always
+before "b"s, and will always have both "g" and "b". Find the index of first "b"
+Or explain it another way: you are giving a revision history, find the first bad
+revision. "g" stands for good revision and "b" stands for bad revision.
 
+Example:
 arr = ["g", "g", "g", "b", "b"]
 return 3 (zero based index)
-
 '''
 
-def find_bad_version(arr):
-
+def find_first_bad_version(arr):
     """
     TC: O(n)
     SC:
         - Input: O(n)
         - Auxiliary: O(1)
         - Total: O(n + 1) => O(n)
-
-    Optimizations ideas:
-        - To use binary search. which will make the algorithm logarithmic complexity.
     """
-    bad = None # 3
-
+    bad = None
     for i, char in enumerate(arr):
         if char == "b":
             bad = i
             break
-
     return bad
 
 
-def find_bad_version_optimized(arr):
-    index = None #
-    left, right = 0, len(arr)-1 #   0, 4
-
+def find_first_bad_version_optimized(arr):
+    """
+    TC: O(log n); using binary search
+    SC:
+        - Input: O(n)
+        - Auxiliary: O(1)
+        - Total: O(n + 1) => O(n)
+    """
+    index = None
+    left, right = 0, len(arr)-1
     while left <= right:
-        mid = (left + right) // 2 # 2
-        mid_element = arr[mid] # b
-
+        mid = (left + right) // 2
+        mid_element = arr[mid]
         if mid_element == "b":
-            # print(mid)
-            index = mid # Store the index temporarily
+            index = mid  # Store the index temporarily
             right = mid - 1
             left += 1
         else:
             left += 1
-
     return index
