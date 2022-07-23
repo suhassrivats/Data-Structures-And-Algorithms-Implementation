b9dd498800abadd0fe7730ab60708ce3d6afd946
suhassrivats <suhassrivats@gmail.com>
2022-07-22 18:33:01 -0700

    LC1472:DesignBrowserHistory

---
+++ Problems/Leetcode/1472_DesignBrowserHistory.py
@@ -0,0 +1,37 @@
+class ListNode:
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+        self.prev = None
+
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.root = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        node = ListNode(url)
+        self.root.next = node
+        node.prev = self.root
+        self.root = self.root.next
+
+    def back(self, steps: int) -> str:
+        while (steps and self.root.prev):
+            self.root = self.root.prev
+            steps -= 1
+        return self.root.val
+
+    def forward(self, steps: int) -> str:
+        while (steps and self.root.next):
+            self.root = self.root.next
+            steps -= 1
+        return self.root.val
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
