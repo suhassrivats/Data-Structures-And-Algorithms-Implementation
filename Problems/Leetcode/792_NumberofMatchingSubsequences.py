0a9c3369ef0029b9d235d853898421044d1c0e90
suhassrivats <suhassrivats@gmail.com>
2022-08-17 23:11:44 -0700

    style: improve comments

--- Problems/Leetcode/792_NumberofMatchingSubsequences.py
+++ Problems/Leetcode/792_NumberofMatchingSubsequences.py
@@ -5,38 +5,47 @@ from bisect import bisect
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         """
+        Idea:
+            - Store chars in s and its occurences in a map. This way we don't have to iterate
+            through entire "s" to find the indices of a particular char.
+            a: [0]
+            b: [1]...
+            - For each char in word, check if the char exists. 
+            - Also, note that we need to keep track of prev index so that next occurence of the same char should be greater than prev-index.
+
+            Example:
+            s = "aabc", w=aba
+            a: [0,1]
+            b: [2]
+            c: [3]
+
+            Here, we can't consider "a" at position 1 as it appears before b.
+
         T: O(N * Mlog(M))
         S: O(N)
         """
+
         lookup = defaultdict(list)
-        result = 0
+        count = 0
 
-        # Store positions of a char
-        for i, c in enumerate(s):
-            lookup[c].append(i)
-
-        def bs(lst, n):
-            l, r = 0, len(lst)
-            while l < r:
-                mid = (l+r)//2
-                if n < lst[mid]:
-                    r = mid
-                else:
-                    l = mid+1
-            return l
+        # char: [list, of, its, locations]
+        for i, s in enumerate(s):
+            lookup[s].append(i)
 
         for word in words:
             prev = -1
             found = True
             for c in word:
-                # tmp = bisect(lookup[c], prev)
-                tmp = bs(lookup[c], prev)
+                # Since we need to find the index greater than our prev, it is best that we use Binary Search
+                # only check in particular char indices, starting from prev index
+                tmp = bisect(lookup[c], prev)
+                # If we have traversed the entire list, then we don't have a subsequence
                 if tmp == len(lookup[c]):
                     found = False
                     break
                 else:
-                    prev = lookup[c][tmp]
+                    prev = lookup[c][tmp]  # update prev index position
             if found:
-                result += 1
+                count += 1
 
-        return result
+        return count
